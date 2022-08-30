84f577720c7307a98e39345f92b6c3d4296595e6
Lucas Pedro <rodrigerioo@gmail.com>
2022-08-30 16:52:36 -0300

    requisito 01

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class login extends Component {
+  render() {
+    return (
+      <div data-testid="page-login">
+        login
+      </div>
+    );
+  }
+}
+
+export default login;
