d4ff0ab9f4df42bde46e013b0b20267e171345e5
Lucas Pedro <rodrigerioo@gmail.com>
2022-08-31 15:14:43 -0300

    requisito 05

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,57 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isButtonDisabled: true,
+      artista: '',
+    };
+  }
+
+  // history push
+
+  handleChange = ({ target }) => {
+    const { name, type } = target;
+    const value = type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    }, () => {
+      const dois = 2;
+      const valueLength = value.length;
+      if (valueLength >= dois) {
+        this.setState({
+          isButtonDisabled: false,
+        });
+      } else {
+        this.setState({
+          isButtonDisabled: true,
+        });
+      }
+    });
+  };
+
   render() {
+    const { isButtonDisabled, artista } = this.state;
+
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            data-testid="search-artist-input"
+            placeholder="Nome do artista/musica"
+            name={ artista }
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ isButtonDisabled }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
